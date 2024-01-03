8fd2a199833cd0584b31dc77787b49c6b5de7bc1
Cynthia-Isutsa <isutsacynthia@gmail.com>
2024-01-03 22:57:19 +0300

    Built in middleware for POST requests

--- myContacts/server.js
+++ myContacts/server.js
@@ -1,9 +1,9 @@
 const express = require("express")
 const dotenv = require("dotenv").config()
-
 const app = express();
-
 const port = process.env.PORT || 5000
+
+app.use(express.json())
 
 app.use('/api/contacts', require('./routes/contactRoutes'))
 
