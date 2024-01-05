def879a4c7efbed752d4a7a18eeb69b63af270a0
Cynthia-Isutsa <isutsacynthia@gmail.com>
2024-01-06 01:54:54 +0300

    Error handling midware

--- myContacts/controllers/contactController.js
+++ myContacts/controllers/contactController.js
@@ -20,6 +20,11 @@ const getContact = (req, res) =>{
 
 const createContact = (req, res) =>{
     console.log("The request body is:" , req.body)
+    const {name, email, phone} = req.body
+    if(!name || !email || !phone){
+      res.status(400)
+      throw new Error("All Fields are Mandatory")
+    }
     res.status(201).json({message: 'Create contact'})
   }
 
